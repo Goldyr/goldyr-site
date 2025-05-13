11b836a3b87c8b7523f1d424d618b7ec0526f6ed
goldyr <enzoefb@hotmail.com>
2025-05-13 13:41:55 -0300

    lil decoration and tests for blogstitles

--- src/blog_titles.js
+++ src/blog_titles.js
@@ -6,9 +6,12 @@ const base_URL = "../blogs/";
 const getTitles = async () => {
   console.log("getTitles");
   try {
-    const github_response = await fetch(github_repo_URL);
-    const github_response_json = await github_response.json();
-    const tbody = document.getElementById("placeholder1");
+    //const github_response = await fetch(github_repo_URL);
+    //const github_response_json = await github_response.json();
+    //console.log(github_response_json)
+    const github_response_json = [{ name: "name_test_large_extra_extra" }, { name: "asfsaogfsahgoianame_test_1" }, { name: "name_test_1" }, { name: "ohrnhognw" }, { name: "giheheo" }, { name: "m1-gm1301-03" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "aa" }, { name: "bNBBOSJBOIDJ" }, { name: "name_test_1" }, { name: "georihnwio" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "name_test_1" },]
+
+    const tbody = document.getElementById("blogs_titles");
 
     github_response_json.forEach((title) => {
       let tr = document.createElement("tr");
@@ -17,7 +20,7 @@ const getTitles = async () => {
       //let p = document.createElement("p");
       let a = document.createElement("a");
 
-      div.classList.add("rectangle", "border");
+      //div.classList.add("rectangle");
 
       const name = title.name.split(".md")[0]
       //p.innerText = name;
