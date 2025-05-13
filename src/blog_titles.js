37a1c8ade3669a8c3449ff352a0e0995858f85d1
goldyr <enzoefb@hotmail.com>
2025-05-13 12:28:00 -0300

    bugs, works correctly with local files

--- src/blog_titles.js
+++ src/blog_titles.js
@@ -1,26 +1,43 @@
-const baseURL = "https://api.github.com/repos/Goldyr/public-notes/contents";
+const github_repo_URL = "https://api.github.com/repos/Goldyr/public-notes/contents";
+//WARNING: depending where you host this this might need a small change
+const base_URL = "../blogs/";
+
 
 const getTitles = async () => {
   console.log("getTitles");
   try {
-    const response = await fetch(baseURL);
-    const titles = await response.json();
-    //const body = document.getElementById("body");
-    //const div = document.getElementById("placerholder1");
-    const ul = document.createElement("ul");
-    titles.forEach((title) => {
-      let li = document.createElement("li");
-      li.innerText = `title: ${title.name}`;
-      ul.appendChild(li);
-    });
+    const github_response = await fetch(github_repo_URL);
+    const github_response_json = await github_response.json();
+    const tbody = document.getElementById("placeholder1");
 
-    console.log(titles);
-    /*  div.appendChild(ul); */
-    document.getElementById("placeholder1").appendChild(ul);
+    github_response_json.forEach((title) => {
+      let tr = document.createElement("tr");
+      let td = document.createElement("td");
+      let div = document.createElement("div");
+      //let p = document.createElement("p");
+      let a = document.createElement("a");
+
+      div.classList.add("rectangle", "border");
+
+      const name = title.name.split(".md")[0]
+      //p.innerText = name;
+      //a.innerText = title.download_url;
+      a.innerText = name;
+      //a.href = title.download_url;
+      a.href = `${base_URL}${name}.md`;
+      //div.addEventListener('click', () => window.location.replace(`${base_URL}${name}.html`))
+
+      //div.appendChild(p);
+      div.appendChild(a);
+      td.appendChild(div);
+      tr.appendChild(td);
+
+      tbody.classList.add("table_center")
+      tbody.appendChild(tr);
+    });
   } catch (err) {
     console.error(err);
   }
-
   return;
 };
 
