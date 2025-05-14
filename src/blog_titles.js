bf5b13cdfae931a542869aa583f45fb409c249d5
goldyr <enzoefb@hotmail.com>
2025-05-14 16:19:51 -0300

    individual blog css, blogs titles might need rethinking?, stuff is working tho

--- src/blog_titles.js
+++ src/blog_titles.js
@@ -1,15 +1,13 @@
 const github_repo_URL = "https://api.github.com/repos/Goldyr/public-notes/contents";
 //WARNING: depending where you host this this might need a small change
-const base_URL = "../blogs/";
+const base_URL = "../public/";
 
 
 const getTitles = async () => {
   console.log("getTitles");
   try {
-    //const github_response = await fetch(github_repo_URL);
-    //const github_response_json = await github_response.json();
-    //console.log(github_response_json)
-    const github_response_json = [{ name: "name_test_large_extra_extra" }, { name: "asfsaogfsahgoianame_test_1" }, { name: "name_test_1" }, { name: "ohrnhognw" }, { name: "giheheo" }, { name: "m1-gm1301-03" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "aa" }, { name: "bNBBOSJBOIDJ" }, { name: "name_test_1" }, { name: "georihnwio" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "name_test_1" }, { name: "name_test_1" },]
+    const github_response = await fetch(github_repo_URL);
+    const github_response_json = await github_response.json();
 
     const tbody = document.getElementById("blogs_titles");
 
@@ -27,7 +25,7 @@ const getTitles = async () => {
       //a.innerText = title.download_url;
       a.innerText = name;
       //a.href = title.download_url;
-      a.href = `${base_URL}${name}.md`;
+      a.href = `${base_URL}${name}.html`;
       //div.addEventListener('click', () => window.location.replace(`${base_URL}${name}.html`))
 
       //div.appendChild(p);
